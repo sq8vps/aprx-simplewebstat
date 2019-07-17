480f86ae38ab46f7f5edd2548466ba6c5bcebfa6
IZ7BOJ <38103662+IZ7BOJ@users.noreply.github.com>
2019-07-17 22:18:19 +0200

    Update functions.php
    Added processing of "d" frames
    Bug fix for recognition of timestamp (included also "/" and "h" in addition to "z")

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+/******************************************************************************************
+This file is a part of SIMPLE WEB STATICSTICS GENERATOR FROM APRX LOG FILE
+It's very simple and small APRX statictics generator in PHP. It's parted to smaller files and they will work independent from each other (but you always need chgif.php).
+This script may have a lot of bugs, problems and it's written in very non-efficient way without a lot of good programming rules. But it works for me.
+Author: Peter SQ8VPS, sq8vps[--at--]gmail.com & Alfredo IZ7BOJ
+You can modify this program, but please give a credit to original authors. Program is free for non-commercial use only.
+(C) Peter SQ8VPS & Alfredo IZ7BOJ 2017-2018
+
+*******************************************************************************************/
+
 function stationparse($frame) //function for parsing station information
 {
 	global $receivedstations;
@@ -11,15 +21,23 @@ function stationparse($frame) //function for parsing station information
 	global $time;
 	global $cntalias;
 	$fg = 0;
-	if(strpos($frame, $callraw." R")) //if frame received by RF
+
+	if((strpos($frame, $callraw." R"))OR(strpos($frame, $callraw." d")))//if frame received by RF
 	{
 		$uu = substr($frame, 0, 19);  //take only the part of the line, where date and time is
 		$uu = strtotime($uu) + date('Z'); //convert string with date and time to the Unix timestamp and add a timezone shift
 		if($uu > $time) //if frame was received in out time range
 		{
 			$aa = explode(">", $frame); //divide frame from > separator to get station's callsign
-			$stationcall = substr($aa[0], strpos($aa[0], $callraw." R ") + strlen($callraw." R ")); //remove date and time, interface call up to the received station's call, so that we get only station's call
-			
+			if(strpos($aa[0]," R ")) //if it's received frame
+				{
+				$stationcall = substr($aa[0], strpos($aa[0], $callraw." R ") + strlen($callraw." R ")); //remove date and time, interface call up to the received station's call, so that we get only station's call
+				}
+			if(strpos($aa[0]," d ")) //if it's a "d" frame
+				{
+				$stationcall = substr($aa[0], strpos($aa[0], $callraw." d *") + strlen($callraw." d *")); //remove date and time, interface call up to the received station's call, so that we get only station's call
+				}
+
 			if(array_key_exists($stationcall, $receivedstations)) //if this callsign is already on stations list
 			{
 				$receivedstations[$stationcall]++; //increment the number of frames from this station
@@ -27,16 +45,16 @@ function stationparse($frame) //function for parsing station information
 			{
 				$receivedstations[$stationcall] = 1; //add callsign to the list
 			}
-			
+
 			$bb = substr($frame, 46); //let's cut temporarily some part of a frame to make sure, that there is no : character, because we want it only as a separator between frame path and info field
 			//------DEBUG-----^^^^^^ this can make some problems, beacuse it's very primitive
 			$bb = substr($bb, strpos($bb, ":") + 1); //get whole date from the frame after a : character, to get info field
-			if(($bb[0] === "@") or ($bb[0] === "!") or ($bb[0] === "=") or ($bb[0] === "/") or (ord($bb[0]) === 96) or (ord($bb[0]) === 39)) //if it's a frame with position or Mic-E position			
+			if(($bb[0] === "@") or ($bb[0] === "!") or ($bb[0] === "=") or ($bb[0] === "/") or (ord($bb[0]) === 96) or (ord($bb[0]) === 39)) //if it's a frame with position or Mic-E position
 			{
 				if($bb[7] === 'z') //if the positions contains timestamp shift reading symbol data by 7 characters
 				{
 					$fg = 26;
-				} else 
+				} else
 				{
 					$fg = 19;
 				}
@@ -55,7 +73,7 @@ function stationparse($frame) //function for parsing station information
 						{
 							$staticstations[] = $stationcall;
 						}
-					}	
+					}
 					elseif(in_array($bb[$fg], array('$', '\'', '(', ')', '*', '<', '=', '>', 'C', 'F', 'P', 'R', 'U', 'X', 'Y', '[', '^', 'a', 'b', 'f', 'g', 'j', 'k', 'p', 's', 'u', 'v')))
 					{
 						if(!in_array($stationcall, $movingstations))
@@ -69,9 +87,9 @@ function stationparse($frame) //function for parsing station information
 						{
 							$otherstations[] = $stationcall;
 						}
-					}	
+					}
 			}
-			
+
 			$cc = substr($frame, strpos($frame, ">")); //temporarily get everything after > symbol (after received station callsign)
 			$cc = substr($cc, 0, strpos($cc, ":")); //and then everything before info field separator, so that we have only frame path right now
 			if(strpos($cc, '*') !== false) //if there is a * the frame was definitely not heard directly
@@ -87,8 +105,8 @@ function stationparse($frame) //function for parsing station information
 						if(!in_array($stationcall, $viastations))
 						{
 							$viastations[] = $stationcall;
-						}	
-						return;	
+						}
+						return;
 				}
 				$cntpos = strpos($cc, $cntalias);
 				if((strpos($cc, $cntalias) !== false) and ($cc[$cntpos + 3] == "-")) //if there is national untraced alias without *, the frame still can be heard indirectly
@@ -116,9 +134,9 @@ function stationparse($frame) //function for parsing station information
 			}
 		}
 	}
-	
-}
 
+}
+/*
 function load($frame, $end)
 {
 	global $framespermin;
@@ -134,9 +152,9 @@ function load($frame, $end)
 		$time2 = strtotime($time2);
 		$framespermin = 20 / (($time2 - $time1) / 60);
 	}
-	
-}
 
+}
+*/
 function nmeatodec($data, $shift)
 {
 	$dec = 0;
@@ -238,29 +256,29 @@ function mice_decode($dest, $info)
     $ghf -= 28;
     $declon += $ghf;
 
-	
+
     $declon = $declon * $lonneg;
-	
+
 
 	//converting DDMMmm to DDdddddd
 	//latitude
 	$tt = 0;
 	$tt += (int)($declat / 10000);
-	
+
 	$temp = ($declat % 10000) / 100;
 	$temp /= 60;
-	
+
 	$declat = $tt + $temp;
-	
-	
+
+
 	//longtitude
 	$tt = 0;
 	$tt += (int)($declon / 10000);
-	
-	
+
+
 	$temp = ($declon % 10000) / 100;
 	$temp /= 60;
-	
+
 	$declon = $tt + $temp;
 
 }
@@ -291,85 +309,87 @@ function frameparse($frame)
 	global $othertime;
 	global $mice;
 	global $bearing;
+	global $device;
 
 		$packet = substr($frame, 36); //get only frame, without interface call, date etc.
 		$aa = explode(">", $packet); //get the callsign
-		
+		if(substr($aa[0],0,1)=="*") {
+		$aa[0]=str_replace("*","",$aa[0]);
+			}
 		if($aa[0] == $scall)
 		{
-			
+
 			$noofframes++;
 			if($posframefound and $otherframefound) return;
-			$bb = explode(":", $packet); //get only info field, so everything after : separator
-			
+			$bb = explode(":", $packet,2); //get only info field, so everything after : separator
+
 			$bb = str_replace("<0x1c>", chr(28), $bb); //replace unprintable characters written as <0xAA> with it's real value
 			$bb = str_replace("<0x1d>", chr(29), $bb); //replace unprintable characters written as <0xAA> with it's real value
 			$bb = str_replace("<0x1e>", chr(30), $bb); //replace unprintable characters written as <0xAA> with it's real value
 			$bb = str_replace("<0x1f>", chr(31), $bb); //replace unprintable characters written as <0xAA> with it's real value
 			$bb = str_replace("<0x7f>", chr(127), $bb); //replace unprintable characters written as <0xAA> with it's real value
-			
+
 			$dd = substr($bb[1], 0); //i have no idea, but i must do this, because without this there are some problems
-							
+
 			if(($dd[0] === "@") or ($dd[0] === "!") or ($dd[0] === "=") or ($dd[0] === "/") or (ord($dd[0]) === 96) or (ord($dd[0]) === 39)) //if it's a frame with position or Mic-E position	
 			{
-				
+
 				if($posframefound) return; //if we have already position frame parsed, just skip it
-			
-				
-			
+
+
 				$posframe = $packet; //save whole posistion frame
-				
+
 				$posdate = substr($frame, 0, 10); //extract date
 				$postime = substr($frame, 11, 8); //extract time
-				
+
 				$path = explode(">", $bb[0]); //take everything after station callsign and before info field (see bb[0])
 				$lastpath = $path[1]; //take only path part
-				
+
 				$posframefound = 1; //newest position frame found
-				
+
 				if((ord($dd[0]) === 96) or (ord($dd[0]) === 39)) //if it's a Mic-E frame
 				{
 					$mice = 1;
-					
+
 					$symboltab = $dd[8];
 					$symbol = $dd[7];
-					
+
 					$destaddr = explode(",", $aa[1]); //get destination address, which encodes latitude
-					
+
 					mice_decode($destaddr[0], $bb[1]);
-					
+
 					$comment = substr($bb[1], 9);
-					
+
 				}
 				else //if it's a standard frame
 				{
 					$mice = 0;
-					if($dd[7] === 'z') //if the positions contains timestamp
+					if(($dd[7] === 'z')OR($dd[7] === '/')OR($dd[7] === 'h')) //if the positions contains timestamp
 					{
 						$symboltab = $dd[16];
 						$symbol = $dd[26];
 						$comment = substr($dd, 27);
-						
+
 						$shft = 7;
 
-					} else 
+					} else
 					{
 						$symboltab = $dd[9];
 						$symbol = $dd[19];
 						$comment = substr($dd, 20);
 						$shft = 0;
-					}	
+					 }
 
 					//convert NMEA to decimal degrees
 					$declat = nmeatodec($dd, 1 + $shft);
 					if($dd[8 + $shft] == 'S') $declat *= -1;
 					$declon = nmeatodec($dd, 11 + $shft);
 					if($dd[18 + $shft] == 'W') $declon *= -1;
-						
+
 
 				}
-				
-				//haversine formula for distance calculation	
+
+				//haversine formula for distance calculation
 				$latFrom = deg2rad($stationlat);
 				$lonFrom = deg2rad($stationlon);
 				$latTo = deg2rad($declat);
@@ -377,35 +397,37 @@ function frameparse($frame)
 
 				$latDelta = $latTo - $latFrom;
 				$lonDelta = $lonTo - $lonFrom;
-				
+
 				$bearing = rad2deg(atan2(sin($lonDelta)*cos($latTo), cos($latFrom)*sin($latTo)-sin($latFrom)*cos($latTo)*cos($latDelta)));
 				if($bearing < 0) $bearing += 360;
 
 				$angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
 				cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
 				$distance = round($angle * 6371, 2); //gives result in km rounded to 2 digits after comma
-				
+
 				$declat = round($declat, 5);
 				$declon = round($declon, 5);
 				$bearing = round($bearing, 1);
-									
+
 			}
-			else if(($dd[0] === ">") or ($dd[0] === "<") or ($dd[0] === "{")) //if it's a status or beacon frame
+			else
+//			else if(($dd[0] === ">") or ($dd[0] === "<") or ($dd[0] === "{")) //if it's a status or beacon frame
 			{
 				if($otherframefound) return; //if we have already status frame parsed, just skip it
-			
+
 				$otherframe = $packet; //save whole status frame
-				
+
 				$status = substr($dd, 1);
-				
+
 				$otherdate = substr($frame, 0, 10); //extract date
 				$othertime = substr($frame, 11, 8); //extract time
-				
+
 				$otherframefound = 1; //newest beacon frame found
 			}
-		}
 
-}
+	} //close if aa=call
+
+} //close frameparse function
 
 //function for load calc
 
@@ -422,6 +444,7 @@ while (($index1<$lines)AND(!((strpos($logfile[$lines - $index1],$callraw." R"))O
         $index1++;
         }
 $time1 = strtotime(substr($logfile[$lines - $index1], 0, 19));
+
 $index2=$index1+1;
 
 //go back to last-20  received packets and take time
@@ -430,13 +453,13 @@ while (($index2<$lines)AND($count<19)) {
                 $time2 = strtotime(substr($logfile[$lines - $index2], 0, 19));
                 $count++;
                 }
-        $index2++;
+	$index2++;
 }
 $rxframespermin = $count / (($time1 - $time2) / 60);
 //echo $count."<br>";//debug line
 //echo $index1."<br>";//debug line
 //echo $index2."<br>";//debug line
-return(rxframespermin);
+return $rxframespermin;
 }
 
 //maybe it's possible to merge these two functions...
@@ -469,7 +492,7 @@ $txframespermin = $count / (($time1 - $time2) / 60);
 //echo $count."<br>"; // debug line
 //echo $index1."<br>"; //debug line
 //echo $index2."<br>"; //debug line
-return(txframespermin);
+return $txframespermin;
 }
 
 function device() {
@@ -493,7 +516,7 @@ function device() {
 						$tocline=$tocfile[$toclines];
 						$toc = substr($tocline,6,strpos(substr($tocline,6),"  "));//cut tocall from file line
 						//compare destination call + wilds with all file lines. Wildcards can be "n","x","y"
-						if((strpos($toc, $tocall2.str_repeat("x",$w)) !== false)OR(strpos($toc, $tocall2.str_repeat("n",$w)!== false))OR(strpos($toc, $tocall2.str_repeat("y",$w)) !== false)) {								$match=1;
+						if((strpos($toc, $tocall2.str_repeat("x",$w)) !== false)OR(strpos($toc, $tocall2.str_repeat("n",$w))!== false)OR(strpos($toc, $tocall2.str_repeat("y",$w)) !== false)) {								$match=1;
 								break; //exit of match
 								}
 						$toclines++; //otherwise go to next file line
