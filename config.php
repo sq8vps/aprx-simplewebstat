4be0a92c0c113e45cc74a15f08a5f3626d02c5ab
Alfredo Vania - IZ7BOJ <38103662+IZ7BOJ@users.noreply.github.com>
2021-03-12 11:02:44 +0100

    Update config.php (#29)
    Added interfaces callsigns and description which will be read by chgif.php

--- config.php
+++ config.php
@@ -7,10 +7,15 @@ $logpath = "aprx-rf.log"; //full path to the aprx-rf.log file
 $confpath = "aprx.conf"; //full path to the aprx.conf file
 $cntalias = "IT"; //your national APRS untraced digipeater alias (helps checking, if frame was received directly or not), for ex. SP is in Poland, HU is in Hungary. If you don't know, what is it, you can leave it blank.
 
+//interface callsigns. Insert callisgns between "" and separated by commas 
+$interfaces=array("IZ7BOJ-11","IZ7BOJ-12");
+
+//interface descriptions. Insert strings between "" and separated by commas
+$intdesc=array("144.800  VHF Port","432.500 UHF Port");
 
 //interface callsign also can be changed temporarily from the website, but this option provides automatic choose of the interface, instead of choosing it by hand via website every time
 $static_if = 1; //1 to enable using static interface callsign
-$static_call = "IQ7NK-11"; //interface callsign to be set as default, with SSID
+$static_call = "IZ7BOJ-11"; //interface callsign to be set as default, with SSID
 $static_lang = "en"; //language to be set by default (it can be changed temporarily via website): en - English, pl - Polski
 
 //station posistion data for calculating distance from received station in details.php 
